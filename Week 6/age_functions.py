862b69b233bc280c229d241e6fe3566d4550ed95
Robert Kreml <kreml006@umn.edu>
2019-10-09 14:56:44 -0500

    Adding Age Functions File

---
+++ Week 6/age_functions.py
@@ -0,0 +1,29 @@
+# START WITH old functions:
+def age_convert(num, inUnits = "y", outUnits = "m"):
+    # these are NESTED if statements
+    # remember our nested flowchart
+    if inUnits is "y":
+        if outUnits is "y":
+            return(num) # if both are y, just return num
+        elif outUnits is "m":
+            return(num * 12) # if y to m, multiply by 12
+        else: # otherwise, error for outUnits
+            raise ValueError("outUnits must be 'y' or 'm'")
+    elif inUnits is "m":
+        if outUnits is "y": # if m to y, divide by 12
+            return(num / 12)
+        elif outUnits is "m": # if both m, just return num
+            return(num)
+        else: # otherwise, error for outUnits
+            raise ValueError("outUnits must be 'y' or 'm'")
+    else: # otherwise, error for inUnits
+        raise ValueError("inUnits must be 'y' or 'm'")
+
+# DEFINE a function called 'mean', that takes 1 argument:
+#   nums: a list of numbers to find the mean of
+def mean(nums):
+    # here, let's calculate the mean
+    sum_nums = sum(nums) # find the sum
+    n_nums = len(nums) # and the number (length) in list
+    mean_nums = sum_nums / n_nums # then compute arithmetic mean
+    return(mean_nums) # return
