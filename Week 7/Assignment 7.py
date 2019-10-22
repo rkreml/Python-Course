a84b94ee1bc48580f6a8e7eeb2b633ef23c45e34
Robert Kreml <kreml006@umn.edu>
2019-10-21 21:12:37 -0500

    Assignment 7 Progress

--- Week 7/Assignment 7.py
+++ Week 7/Assignment 7.py
@@ -31,7 +31,7 @@ import numpy as np
 #       In other words, I should be able to understand what the variable is from reading its name.
 #       Also add a comment that fully describes what the IV represents.
 
-hours_studied = float([1.0, 9.0, 12.5, 19.8, 26.7, 33.1, 2.8, 9.7, 10.8, 9.9])
+hours_studied = [float(1.2), float(9.6), float(12.5), float(19.8), float(26.7), float(33.1), float(2.8), float(9.7), float(10.8), float(9.9)]
 # This IV is the number of hours studied as a float, which means the data is rounded to the nearest tenth of an hour.
 
 ## 1b. Next, create a LIST of data for 10 participants' values for the CATEGORICAL IV.
@@ -44,7 +44,7 @@ hours_studied = float([1.0, 9.0, 12.5, 19.8, 26.7, 33.1, 2.8, 9.7, 10.8, 9.9])
 #       Also add a comment that fully describes what the IV represents.
 #           E.g., "0" is Control, "1" is Experimental, etc.
 
-type_of_studying = int([1, 1, 1, 2, 0, 1, 1, 0, 1, 2])
+type_of_studying = [int(1), int(1), int(1), int(2), int(0), int(1), int(1), int(0), int(1), int(2)]
 # These are the types of studying that can be done. The numbers correspond into a cateogry type. 0 means the student used the study guide. 1 means the student went to the study session. And 2 means the student came to office hours.
 
 ## 1c. Now create a LIST of participants' 10 DV values (e.g., scores on an exam)
@@ -54,7 +54,7 @@ type_of_studying = int([1, 1, 1, 2, 0, 1, 1, 0, 1, 2])
 #       In other words, the 2nd # in the IV list should correspond to the 2nd # in the DV list.
 #       Also add a comment that fully describes what the DV represents.
 
-exam_scores = int([32, 55, 65, 79, 93, 99, 43, 57, 68, 71])
+exam_scores = [int(32), int(55), int(65), int(79), int(93), int(99), int(43), int(57), int(68), int(71)]
 # These are the exam scores out of a possible 100 points.
 
 ## 1d. Now put all 3 variables, along with a participant ID/# list (1-10), into a single NUMPY ARRAY.
@@ -70,7 +70,7 @@ data = np.column_stack([subj_num, hours_studied, type_of_studying, exam_scores])
 #       What happened to your categorical IV? Subject number?  (Answer below as a comment.)
 
 print(data)
-
+# The categorical IV and Subject number both have decimals in it. The array probably caused the data to use float as it is the most selective type of data.
 
 #############
 ### PROBLEM 2
@@ -78,18 +78,42 @@ print(data)
 ## 2a. Demonstrate two DIFFERENT ways to return the first 4 rows of data from your array.
 #          By "different ways", I mean you must type anything differently (it can be similar in concept).
 
+print("The first way to get the first four rows of our data array is to use data[0:4,]")
+print(data[0:4,])
+
+print("The second way to get the first four rows of our data array is to use a simple loop.")
+
+i = 0
+while i <= 3:
+    print(data[i,])
+    i += 1
+
 ## 2b. Index your DV column of the data frame using its column index.
+
+print("The Dependent Variable column of the data array is:")
+print(data[:,3])
 
 ## 2c. Index your array to return just the DV for participant #4 only.
 #       In other words, you'll be indexing both dimensions and should get ONE number back.
 
+print("The dependent variable for participant 4 is:")
+print(data[3,3])
+
 ## 2d. Slice your numpy array so that you get all of the data for participant #5.
 
+print("The data for participant 5 is:")
+print(data[4,:])
+
 ## 2e. Slice your numpy array so that you get BOTH IVs (but NO DVs) for participants #7-9.
+
+print("The data for both independent variables for participants 7, 8, and 9 is:")
+print(data[6:9,1:3])
 
 ## 2f. Slice your numpy array so that you get ONLY the categorical IV and the DV
 #       and only for participants #3 and #6. (The result should be a 2x2 array.)
 
+print("The categorical independent variable and dependent variable for participants 3 and 6 is:")
+print(data[[[2,2], [5,5]], [2,3]])
 
 #############
 ### PROBLEM 3
@@ -97,10 +121,21 @@ print(data)
 ## 3a. Use the np.mean() function to find the mean of the DV.
 #       Save the DV mean as a variable with a meaningful and readable name, with a comment.
 
+print("The mean of the dependent variable of the data array is:")
+dv_mean = float(np.mean(data[:,3]))
+print(dv_mean)
+
+# This is the mean value of the dependent variable as a float.
+
 
 ## 3b. Use the np.mean() function to find the mean of the continuous IV.
 #       Save the continuous IV mean as a variable with a meaningful and readable name, with a comment.
 
+print("The mean of the continuous independent variable of the data array is:")
+civ_mean = float(np.mean(data[:,1]))
+print(civ_mean)
+
+# This is the mean value of the continuous independent variable as a float.
 
 #############
 ### PROBLEM 4
@@ -141,3 +176,4 @@ print(data)
 
 #############
 ### MISTAKE DOCUMENTATION
+# Getting a 2x2 matrix in 2f was very difficult to do. I played around with different attempts of indexing and slicing the array until I got the solution.
